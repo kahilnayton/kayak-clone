4ac9ed21cb6570a12e4e132b0bcdc51f6bd7e7f7
kahil nayton <kahilnayton@gmail.com>
2020-01-04 12:41:58 -0500

    added the flex style to the nav bar

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
 import React from "react";
-import triangleLogo from './img/triangle-logo.png'
 import "./sass/main.scss";
 
+// Assets
+import triangleLogo from "./img/triangle-logo.png";
+import userImage from "./img/user-image.png";
+
+import PlaneIcon from "./Icons/Airplane";
+import Bookmark from "./Icons/Bookmark";
+import Bubbles from "./Icons/Bubbles";
+import Chevron from "./Icons/ChevronThinRight";
+import Heart from "./Icons/Heart";
+import Search from "./Icons/Search";
+import Key from "./Icons/Key";
+
 export default class App extends React.Component {
-  state = {}
+  state = {};
 
   render() {
     return (
       <div className="container">
         <header className="header">
           <img src={triangleLogo} alt="logo" className="logo" />
-          <form
-            action=""
-            typle="text"
-            className="search__input"
-            placeholder="Search hotels"
-          >
+
+          <form action="#" className="search">
+            <input
+              className="search__input"
+              type="text"
+              placeholder="Search hotels"
+            />
             <button className="search__button">
-              {/* <svg className="search-icon">
-                <use xlink:href={sprite}></use>
-              </svg> */}
-
+              <Search width="40px" height="40px" className="search__icon" />
             </button>
-
-
           </form>
-        <div className="content">
-            <nav className="sidebar">navigation</nav>
 
-            <main className="hotel-view">Hotel view</main>
-          </div>
+          <nav className="user-nav">
+            <div className="user-nav__icon-box">
+              <Heart className="user-nav__icon" />
+              <span className="user-nav__notification">7</span>
+            </div>
+
+            <div className="user-nav__icon-box">
+              <Key className="user-nav__icon" />
+              <span className="user-nav__notification">1</span>
+            </div>
+
+            <div className="user-nav__user">
+              <img src={userImage} alt="" className="user-nav__user-photo" />
+              <span className="user-nav__user-name">Kahil</span>
+            </div>
+          </nav>
         </header>
+
+        <div className="content">
+          <nav className="sidebar">navigation</nav>
+
+          <main className="hotel-view">Hotel view</main>
+        </div>
       </div>
     );
   }
 }
-
-
---
+++ src/Icons/Home3.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SvgHome3(props) {
+  return (
+    <svg width={32} height={32} {...props}>
+      <path d="M32 19l-6-6V4h-4v5l-6-6L0 19v1h4v10h10v-6h4v6h10V20h4z" />
+    </svg>
+  );
+}
+
+export default SvgHome3;
